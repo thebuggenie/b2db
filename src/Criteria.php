58199572181c04f88e08b09a864e94d976e1ca07
Daniel André Eikeland <zegenie@gmail.com>
2014-12-10 14:35:11 +0100

    Make sure joined tables with multiple join criteria values are joined with value DB_IN not DB_EQUALS

--- src/Criteria.php
+++ src/Criteria.php
@@ -851,6 +851,8 @@
 
             if (is_null($part['value']) && !in_array($part['operator'], array(self::DB_IS_NOT_NULL, self::DB_IS_NULL))) {
                 $part['operator'] = ($part['operator'] == self::DB_EQUALS) ? self::DB_IS_NULL : self::DB_IS_NOT_NULL;
+            } elseif (is_array($part['value'])) {
+                $part['operator'] = self::DB_IN;
             }
             $sql .= ' ' . $part['operator'] . ' ';
 
